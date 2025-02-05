938a706c465dfb71d6f1078cb8ad7ee6b09990ee
Danilo Azevedo <danilo.ann@gmail.com>
2025-02-04 23:55:31 -0300

    Verbos Http

---
+++ 03_RestASPNET_Verbos/RestASP-NET/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestASP_NET.Model;
+
+namespace RestASP_NET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long i);
+    }
+}
---
+++ 03_RestASPNET_Verbos/RestASP-NET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,66 @@
+﻿using RestASP_NET.Model;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RestASP_NET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long i)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+        
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Danilo",
+                LastName = "Azevedo",
+                Adress = "Caetité-BA",
+                Gender = "Masculino"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Adress = "Some Adess" + i,
+                Gender = "Masculino"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
