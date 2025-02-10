8806e4ff602924886e5221bae470b627493c63bb
Danilo Azevedo <danilo.ann@gmail.com>
2025-02-09 21:46:30 -0300

    Conectando com banco de dados

--- 03_RestASPNET_Verbos/RestASP-NET/Program.cs
+++ 03_RestASPNET_Verbos/RestASP-NET/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using RestASP_NET.Model.Context;
 using RestASP_NET.Services;
 using RestASP_NET.Services.Implementations;
 
@@ -9,6 +11,12 @@ builder.Services.AddControllers();
 
 //Dependency Injection
 builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+var serverVersion = new MySqlServerVersion(new Version(9, 2, 0));
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, serverVersion));
 
 var app = builder.Build();
 
--- 03_RestASPNET_Verbos/RestASP-NET/Services/Implementations/PersonServiceImplementation.cs
+++ 03_RestASPNET_Verbos/RestASP-NET/Services/Implementations/PersonServiceImplementation.cs
@@ -1,4 +1,6 @@
 ﻿using RestASP_NET.Model;
+using RestASP_NET.Model.Context;
+using System;
 using System.Reflection;
 using System.Reflection.Metadata.Ecma335;
 
@@ -6,61 +8,79 @@ namespace RestASP_NET.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
             return person;
         }
 
-        public void Delete(long i)
+        public void Delete(long id)
         {
-            
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
         }
 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-            for (int i = 0; i < 8; i++)
-            {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            }
-            return persons;
+            return _context.Persons.ToList();
         }
         
-        public Person FindById(long id)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Danilo",
-                LastName = "Azevedo",
-                Adress = "Caetité-BA",
-                Gender = "Masculino"
-            };
+        public Person FindById(long id){
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public Person Update(Person person)
         {
+            if(!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if(result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+
             return person;
         }
 
-        private Person MockPerson(int i)
+        private bool Exists(long id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name" + i,
-                LastName = "Person LastName" + i,
-                Adress = "Some Adess" + i,
-                Gender = "Masculino"
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
